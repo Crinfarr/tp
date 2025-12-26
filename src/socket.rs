4a30cad924a4ea5a4070d345759a97c01b15d76d
Asher Hoeveler <theargon0@gmail.com>
2025-12-26 04:58:41 -0600

    More crossplatform socket work

--- src/socket.rs
+++ src/socket.rs
@@ -3,23 +3,13 @@ use crate::errors::IpcServerErr;
 use tokio::net::{UnixListener, UnixStream};
 //https://docs.rs/tokio/latest/tokio/net/struct.UnixStream.html
 
-#[cfg(target_os = "windows")]
-use tokio::net::windows::NamedPipe;
-//https://docs.rs/tokio/latest/tokio/net/windows/named_pipe/index.html
-
-#[cfg(target_os = "windows")]
-pub struct IpcServer {
-    wrapped: Socket,
-}
 #[cfg(not(target_os = "windows"))]
 pub struct IpcServer {
     wrapped_server: tokio::net::UnixListener,
 }
-#[cfg(target_os = "windows")]
-impl IpcServer {}
 #[cfg(not(target_os = "windows"))]
 impl IpcServer {
-    async fn new() -> Result<Self, IpcServerErr> {
+    pub async fn new() -> Result<Self, IpcServerErr> {
         if !std::fs::exists("/run/tp-daemon").map_err(|e| IpcServerErr::IoErr(e))? {
             std::fs::create_dir("/run/tp-daemon").map_err(|e| IpcServerErr::IoErr(e))?;
         }
@@ -28,5 +18,30 @@ impl IpcServer {
         Ok(Self {
             wrapped_server: sock,
         })
+    }
+    pub async fn next_connection(
+        &mut self,
+    ) -> Result<(UnixStream, tokio::net::unix::SocketAddr), IpcServerErr> {
+        Ok(self
+            .wrapped_server
+            .accept()
+            .await
+            .map_err(|e| IpcServerErr::OpenSocketErr(e.to_string()))?)
+    }
+}
+
+#[cfg(target_os = "windows")]
+use tokio::net::windows::NamedPipe;
+//https://docs.rs/tokio/latest/tokio/net/windows/named_pipe/index.html
+
+#[cfg(target_os = "windows")]
+pub struct IpcServer {
+    wrapped: Socket,
+}
+
+#[cfg(target_os = "windows")]
+impl IpcServer {
+    pub async fn new() -> Result<Self, IpcServerErr> {
+        //Need to write this on win fuck
     }
 }
